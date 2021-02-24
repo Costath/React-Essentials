6b54d950f80c73963c64bcf4a85b3d8a4f00e00a
Costath <thalesdaniel@gmail.com>
2021-02-23 22:37:12 -0500

    video 2.4

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 ReactDOM.render(
-  React.createElement("h1", 
-    { style: { 
-        color: "blue"
-      }
-    }, "heyo, let's go"),
+  <ul>
+    <li>Monday</li>
+    <li>Tuesday</li>
+    <li>Wednesday</li>
+    <li>Thursday</li>
+    <li>Friday</li>
+    <li>Saturday</li>
+    <li>Sunday</li>
+  </ul>,
   document.getElementById('root')
 );
