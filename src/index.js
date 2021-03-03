d64283b40c84bf744eaf25f96d5b1f01e4706156
Costath <thalesdaniel@gmail.com>
2021-03-03 11:13:01 -0500

    video 3.6

--- src/index.js
+++ src/index.js
@@ -3,7 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from "./App"
 
+// function AppTwo() {
+//   return <h1>this is the second app</h1>;
+// }
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
 );
+
+
+// ReactDOM.render(
+//   <> {/**short-hand sintaxe for React.Fragment*/}
+//   {/* <React.Fragment> */}
+//     <App />
+//     <AppTwo />
+//   {/* </React.Fragment>, */}
+//   </>,
+//   document.getElementById('root')
+// );
