a5b8c272fba475f0a077adeddf06f41d24b74dd0
Costath <thalesdaniel@gmail.com>
2021-03-16 13:41:27 -0400

    video 6.4

---
+++ src/Checkbox.js
@@ -0,0 +1,21 @@
+import React, { useReducer } from "react";
+
+export function Checkbox() {
+    const [checked, toggle] = useReducer(
+        checked => !checked, // reducer function (taking the current value and returning it inverted)
+        false // initial value
+    );
+    return(
+        <>
+            <label htmlFor="checkbox">
+                {checked ? "checked" : "not checked"}
+            </label>
+            <input 
+                id="checkbox"
+                type="checkbox"
+                value={checked}
+                onChange={toggle}
+            />
+        </>
+    )
+}
